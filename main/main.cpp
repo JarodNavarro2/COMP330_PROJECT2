ccbc495a71bdbd92b73329b9a568018d8b8a53a0
Jarod Navarro <jnavarro2@luc.edu>
2017-09-13 19:11:55 +0000

    Trying to use regular expressions. Test

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <regex>
+#include <vector>
+#include <string>
+
 using namespace std;
+
+
+string find(string key)
+{
+    regex keys("@!#");
+    if (regex_match (key,keys))
+    {
+        cout<<"Key is recognized"<<endl;
+        return key;
+        
+    }
+    else
+    {
+        cout<<"Wrong key or does not exist"<<endl;
+        
+    }
+    return "failed";
+  
+}
 
 int main()
 {
-    cout<<"Working main file"<<endl;
+    //command line for files
+    vector<string> files;
+    cout<<"Enter your files"<<endl;
+    bool flag = true;
+    while (flag)
+    {
+        string enter;
+        cin>>enter;
+        files.push_back(enter);
+        cout<<"contiue? (y/n)"<<endl;
+        string ans;
+        cin>>ans;
+        if (ans == "n" || ans == "N")
+        {
+            flag =false;
+            break;
+        }
+    }
+    cout<<"Enter keyword"<<endl;
+    string key;
+    cin>>key;
+    find(key);
     return 1;
+    
 }
