e6673f789d4546d43f67b735fefc6575c2cfe9b8
Jarod Navarro <jnavarro2@luc.edu>
2017-09-17 19:22:04 +0000

    added command line, along with 3 of 5 functionalities

--- main/main.cpp
+++ main/main.cpp
@@ -8,7 +8,39 @@
 
 using namespace std;
 
+
 //testing the commit of new line of code on existing .cpp file
+/*class notetaking
+{
+  string subject; bool unique; vector<notetaking> notes;
+  public: 
+  void set_name(string);
+  string get_name();
+  void is_unique(bool);
+  bool check_unique();
+  void add_notes(vector<notetaking>);
+  
+};
+void notetaking::set_name(string subject)
+{
+    this.subject=subject;
+}
+void notetaking::is_unique(bool unique)
+{
+    this.unique=unique;
+}
+string notetaking::get_name()
+{
+    return subject;
+}
+bool notetaking::check_unique()
+{
+    return unique;
+}
+void notetaking::add_notes(vector<notetaking> add)
+{
+    notes.push_back(add.begin(), add.end());
+}*/
 string find(string key)
 {
     regex keys("@!#");
@@ -26,54 +58,100 @@ string find(string key)
     return "failed";
   
 }
-
-void read_file(string filename)
+void topic_identifier(string type, vector<string> list)
 {
-    ifstream file;
-    
-    file.open(filename.c_str());
-    
-    if (!file.is_open())
-    {
-        cout<<"Can't open file"<<endl;
-    }
-    else
-    {
-       string store;
-       while (file.peek() != EOF)
-       {
-           getline (file, store);
-           cout<< store;
-       }
-    }
-    file.clear();
-    file.close();
-    
+	if (!type.empty())
+	{
+		for (int i = 0; i < list.size(); i++)
+		{
+			if (find(list.begin(), list.end(), type) != list.end())
+			{
+				cout << "Topic found. Topic: " << type << endl;
+				break;
+			}
+			else
+			{
+				cout << "Topic has not been added to notes" << endl;
+				break;
+			}
+		}
+	}
 }
+
 int main()
 {
     //command line for files
-    vector<string> files;
-    cout<<"Enter your files"<<endl;
-    bool flag = true;
-    while (flag)
-    {
-        string enter;
-        cin>>enter;
-        files.push_back(enter);
-        cout<<"contiue? (y/n)"<<endl;
-        string ans;
-        cin>>ans;
-        if (ans == "n" || ans == "N")
-        {
-            flag =false;
-            break;
-        }
-    }
-    cout<<"Enter keyword"<<endl;
-    string key;
-    cin>>key;
-    find(key);
+   vector<string> filename;
+   vector<string> unique_filename;
+    cout << "Command line active" << endl;
+	string cmd;
+	bool stop = true;
+	while (stop = true)
+	{
+	    break_point:
+	    cmd.empty();
+	    cin >> cmd;
+	    if (!cmd.empty())
+	    {
+			cout << "Command recognized: " << cmd.at(0) << endl;
+			if (cmd.at(0) == '#')
+			{
+				cmd.erase(0, 1);
+				topic_identifier(cmd, filename);
+				goto break_point;
+			}
+			else if (cmd.at(0) == '!')
+			{
+			    cout<<"Command recognized: " << cmd.at(0)<<endl;
+			    cout<<"Enter the name of the file in which you want to make unique: ";
+			    string unique; 
+			    cin>>unique;
+			    if (!unique.empty())
+			    {
+			        for (int i = 0; i < filename.size(); i++)
+			        {
+			            if (find(filename.begin(), filename.end(), unique) != filename.end())
+			            {
+			                cout<<"Topic found. Topic: "<<unique<<endl;
+			                cout<<"Removing from standard list to make it unique..."<<endl;
+			                unique_filename.push_back(unique);
+			                filename.erase(std::remove(filename.begin(), filename.end(), unique), filename.end());
+			                break;
+			            }
+			            else
+			            {
+			              cout<<"Subjcet not added to notes"<<endl; 
+			            }
+			        }
+			    }
+			}
+			else if (cmd == "add")
+			{
+			    while (true)
+			    {
+			        string name;
+			        cout<<"Name of file: ";
+			        cin>> name;
+			        filename.push_back(name);
+			        cout<<"Continue to add?"<<endl;
+			        string ans;
+			        cin>>ans;
+			        if (ans == "no" || ans == "No")
+			        {
+			            break;
+			          
+			        }
+			    }
+			    goto break_point;
+			    
+			}
+			else
+			{
+				cout << "Command " << cmd.at(0) << " has not been implemented" << endl;
+			}
+		
+	    }
+	}
     return 1;
     
 }
